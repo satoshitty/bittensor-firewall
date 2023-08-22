0eee1593cf5f75202f5f48edeb6a2cb0da234d6c
Satoshi's Behind <142260374+satoshitty@users.noreply.github.com>
2023-08-22 00:23:05 -0400

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -3,31 +3,38 @@ import bittensor as bt
 import subprocess
 import toml
 import argparse
+import os
+import grp
 from typing import List, Dict, Set
+
+# Function to run Docker command with or without sudo based on user's permissions
+def run_docker_command(command):
+    # Check if the user is root or in the docker group
+    if os.getuid() == 0 or "docker" in [g.gr_name for g in grp.getgrall() if os.getlogin() in g.gr_mem]:
+        docker_command = ["docker"]
+    else:
+        docker_command = ["sudo", "docker"]
+    # Execute the command
+    subprocess.run(docker_command + command)
 
 # Function to resynchronize with the Bittensor metagraph
 def resync_metagraph(netuids: List[int]) -> Dict[int, List['bt.NeuronInfoLite']]:
     bt.logging.info("resync_metagraph()")
-
+    # Handle single or multiple netuids
     if isinstance(netuids, int): netuids = [netuids]
-
     new_neurons = {}
     for netuid in netuids:
         new_neurons[netuid] = subtensor.neurons_lite(netuid=netuid)
-    
     bt.logging.info("Metagraph synced!")
-
     return new_neurons
 
 # Function to get the IPs of any neurons that have vpermit = True
 def neurons_to_ips(all_neurons: Dict[int, List['bt.NeuronInfoLite']]) -> Set[str]:
     bt.logging.info("neurons_to_ips()")
-
     validator_ips = set()
     for _, subnet_neurons in all_neurons.items():
         ips = [neuron.axon_info.ip for neuron in subnet_neurons if neuron.validator_permit]
         validator_ips.update(set(ips))
-    
     return validator_ips
 
 # Function to whitelist IPs in Traefik
@@ -35,7 +42,6 @@ def whitelist_ips_in_traefik(ips: List[str]):
     # Transform the IPs to a specific format
     ips = ['"' + ip.split(".")[0] + "." + ip.split(".")[1] + '.0.0/16"' for ip in ips]
     ip_list = ', '.join(ips)
-
     # Update the dynamic configuration file
     config = {
         "http": {
@@ -50,10 +56,9 @@ def whitelist_ips_in_traefik(ips: List[str]):
     }
     with open('dynamic_conf/ip_whitelist.toml', 'w') as file:
         toml.dump(config, file)
-
     # Notify Traefik to reload its configuration
-    subprocess.run(["docker", "stop", "traefik"])
-    subprocess.run(["docker", "start", "traefik"])
+    run_docker_command(["stop", "traefik"])
+    run_docker_command(["start", "traefik"])
 
 # Function to parse command line arguments
 def parse_arguments():
@@ -68,23 +73,15 @@ if __name__ == "__main__":
     # Parse command line arguments
     args = parse_arguments()
     # Connect to Bittensor network
-    subtensor: 'bt.subtensor'
-    if args.chain_endpoint:
-        subtensor = bt.subtensor(network="finney", chain_endpoint=args.chain_endpoint)
-    else:
-        subtensor = bt.subtensor(network="finney")
-
+    subtensor = bt.subtensor(network="finney", chain_endpoint=args.chain_endpoint) if args.chain_endpoint else bt.subtensor(network="finney")
     # Infinite loop to keep the metagraph synced and firewall updated
     while True:
         # Resync the metagraph
-        neurons_dict = resync_metagraph(netuids = args.netuid)
-
+        neurons_dict = resync_metagraph(netuids=args.netuid)
         # Get the IPs of any neurons that have vpermit = True
         ips = neurons_to_ips(neurons_dict)
-        
         # Whitelist the IPs in Traefik
         whitelist_ips_in_traefik(ips)
-        
         # Wait for N blocks (default approximately 1200 seconds or 20 minutes)
         bt.logging.info(f"Waiting for {args.sleep_blocks} blocks, sleeping")
         time.sleep(int(args.sleep_blocks*12))
