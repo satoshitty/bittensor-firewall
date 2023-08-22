470df25e9e6724e727c6f42be7b21f0328467f0c
Satoshi's Behind <142260374+satoshitty@users.noreply.github.com>
2023-08-22 14:08:28 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,19 +7,21 @@ else
   DOCKER_COMMAND="sudo docker"
 fi
 
-# Path to the traefik.toml file
-TRAEFIK_CONFIG_PATH="traefik.toml"
+# Stop the container if it exists
+$DOCKER_COMMAND stop myapp
+$DOCKER_COMMAND rm myapp
 
-# Get the gateway IP address of the Docker bridge network
-GATEWAY_IP=$($DOCKER_COMMAND network inspect bridge --format '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
+# Start your own docker application container
+$DOCKER_COMMAND run -d \
+    -p 8081:8080 \
+    --name myapp \
+    -l "traefik.enable=true" \
+    -l 'traefik.http.routers.myapp.rule=PathPrefix(`/`)' \
+    -l "traefik.http.services.myapp.loadbalancer.server.port=8080" \
+    -l "traefik.http.routers.myapp.middlewares=myipwhitelist@file" \
+    hello-world-image # docker image name
 
-# Replace GATEWAY_IP in the traefik.toml file
-sed -i "s/GATEWAY_IP/$GATEWAY_IP/g" $TRAEFIK_CONFIG_PATH
-
-# Replace PORT in the traefik.toml file if not specified
-PORT=8888 # default
-sed -i "s/PORT/$PORT/g" $TRAEFIK_CONFIG_PATH
-
+# Stop the container if it exists
 $DOCKER_COMMAND stop traefik
 $DOCKER_COMMAND rm traefik
 
@@ -27,11 +29,25 @@ $DOCKER_COMMAND rm traefik
 $DOCKER_COMMAND run -d \
   --name traefik \
   --publish 80:80 \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume $PWD/traefik.toml:/etc/traefik/traefik.toml \
   --volume $PWD/dynamic_conf:/etc/traefik/dynamic_conf \
   traefik:latest
 
 # Start the Python script with PM2
-pm2 start firewall.py --name "BittensorFW" --interpreter "$(which python)"
+pm2 start watcher.py --name "BittensorIPWatcher" \
+    --interpreter "$(which python)" \
+    --cron-restart="*/5 * * * *" \
+    --no-autorestart \
+    -- \
+    --wallet.name churchofrao \
+    --wallet.hotkey raovali \
+    --netuid 1
+
+pm2 start firewall.py --name "BittensorFW" \
+    --interpreter "$(which python)" \
+    --cron-restart="*/20 * * * *" \
+    --no-autorestart \
+    -- \
 
 echo "Traefik and Bittensor Firewall are now running!"
